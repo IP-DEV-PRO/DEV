6758d173d4c511d25e5239c97beb5f58ad7b2b9d
Sargis12 <bebe.dinu2000@gmail.com>
2022-05-28 17:17:33 +0300

    OWNER

---
+++ app/src/main/java/com/devpro/activities/CompanyOwnerHomePage.java
@@ -0,0 +1,15 @@
+package com.devpro.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.devpro.R;
+
+public class CompanyOwnerHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_owner_home_page);
+    }
+}
