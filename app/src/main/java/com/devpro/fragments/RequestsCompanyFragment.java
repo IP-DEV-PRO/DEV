aa34269064f40df7641b211626685840672ecb3d
VladuTzZ-sudo <mateiaass@gmail.com>
2022-05-28 23:01:33 +0300

    details page comapnyhomepage

--- app/src/main/java/com/devpro/fragments/RequestsCompanyFragment.java
+++ app/src/main/java/com/devpro/fragments/RequestsCompanyFragment.java
@@ -1,10 +1,10 @@
 package com.devpro.fragments;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,18 +12,21 @@ import androidx.fragment.app.Fragment;
 
 import com.devpro.R;
 
-public class RequestsCompanyFragment  extends Fragment {
+public class RequestsCompanyFragment extends Fragment {
+    Button addLocation, addDescription, addService, changePhoneNumber;
+    Button changeEmail;
+
     public RequestsCompanyFragment() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.company_homepage_requests, container, false);
-
-
-        return rootView;
+        return inflater.inflate(R.layout.company_homepage_requests, container, false);
     }
 
-
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
