f1ca0763eaf8bdb031b2ead1a809f4503a42e958
VladuTzZ-sudo <mateiaass@gmail.com>
2022-05-29 00:16:41 +0300

    details page comapnyhomepage 3

--- app/src/main/java/com/devpro/activities/CompanyAdminHomePageActivity.java
+++ app/src/main/java/com/devpro/activities/CompanyAdminHomePageActivity.java
@@ -1,7 +1,9 @@
 package com.devpro.activities;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +14,35 @@ import com.devpro.fragments.AcceptedCompanyFragment;
 import com.devpro.fragments.EditDetailsCompanyFragment;
 import com.devpro.fragments.RequestsCompanyFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CompanyAdminHomePageActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
+    Button add_location, add_service;
+    TextInputEditText add_description, change_phone, change_email, change_password;
+    String username;
+
+    Fragment requestsFragment;
+    Fragment acceptFragment;
+    Fragment editFragment;
+
+    void setListenersButtons() {
+        //registerButton.setOnClickListener(view -> register(username.getText().toString(),password.getText().toString()));
+        add_location.setOnClickListener(view -> {
+            System.out.println("AMDOSDMOSDOSMDOI0000000000------------------------------------------------------------");
+            //    changeActiviy(RegisterCompanyActivityWithMap.class);
+        });
+
+//        add_service.setOnClickListener(view -> registerUser(username.getText().toString(),password.getText().toString(),
+//                email.getText().toString(),false));
+        //registerCompanyButton.setOnClickListener(view -> changeActiviy(RegisterCompanyActivityWithMap.class));
+    }
+
+    private void changeActiviy(Class activityClass) {
+        Intent myIntent = new Intent(this, activityClass);
+        myIntent.putExtra("key-user", username);
+        startActivity(myIntent);
+    }
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -32,10 +60,28 @@ public class CompanyAdminHomePageActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottom_navigation_company);
 
+        Intent intent = getIntent();
+        username = intent.getStringExtra("key-user");
+
+        Bundle bundle = new Bundle();
+        bundle.putString("key-user", username);
+
         Fragment requestsFragment = new RequestsCompanyFragment();
         Fragment acceptFragment = new AcceptedCompanyFragment();
         Fragment editFragment = new EditDetailsCompanyFragment();
 
+        requestsFragment.setArguments(bundle);
+        acceptFragment.setArguments(bundle);
+        editFragment.setArguments(bundle);
+
+        // add_location = editFragment.requireView().findViewById(R.id.company_add_location_button);
+       /* add_service = findViewById(R.id.company_add_service);
+        add_description = findViewById(R.id.add_description_text);
+        change_phone = findViewById(R.id.add_phone_textInput);
+        change_email = findViewById(R.id.add_email_Input);
+        change_password = findViewById(R.id.add_password_input);
+*/
+        //setListenersButtons();
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
@@ -69,5 +115,9 @@ public class CompanyAdminHomePageActivity extends AppCompatActivity {
         //editare informatii
         //cereri
         //cereri acceptare
+    }
+
+    public String returnUsername() {
+        return username;
     }
 }
--- app/src/main/java/com/devpro/fragments/EditDetailsCompanyFragment.java
+++ app/src/main/java/com/devpro/fragments/EditDetailsCompanyFragment.java
@@ -1,17 +1,25 @@
 package com.devpro.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.devpro.R;
+import com.devpro.activities.CompanyAdminHomePageActivity;
+import com.devpro.activities.RegisterCompanyActivityWithMap;
+
+import java.util.Objects;
 
 public class EditDetailsCompanyFragment   extends Fragment {
+    Button add_location, add_service;
+    String username;
     public EditDetailsCompanyFragment() {
     }
 
@@ -19,10 +27,36 @@ public class EditDetailsCompanyFragment   extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.company_homepage_editdetails, container, false);
-
-
         return rootView;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        add_location = requireActivity().findViewById(R.id.company_add_location_button);
+        add_service = requireActivity().findViewById(R.id.company_add_service);
+
+        username = ((CompanyAdminHomePageActivity) requireActivity()).returnUsername();
+
+        setListenersButtons();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void setListenersButtons() {
+        add_location.setOnClickListener(view -> {
+            //System.out.println("AMDOSDMOSDOSMDOI0000000000------------------------------------------------------------");
+            changeActiviy(RegisterCompanyActivityWithMap.class);
+        });
+    }
+
+    private void changeActiviy(Class activityClass) {
+        Intent myIntent = new Intent(getActivity(), activityClass);
+        myIntent.putExtra("key-user", username);
+        startActivity(myIntent);
+    }
 }
