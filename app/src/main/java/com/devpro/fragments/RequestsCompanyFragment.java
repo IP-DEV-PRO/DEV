b6ef2ab8300e8ce8dfaaf69b5bc836cc8722ea2a
VladuTzZ-sudo <mateiaass@gmail.com>
2022-06-01 16:27:10 +0300

    Matei add_location_company_almost_finished 8

--- app/src/main/java/com/devpro/fragments/RequestsCompanyFragment.java
+++ app/src/main/java/com/devpro/fragments/RequestsCompanyFragment.java
@@ -2,19 +2,20 @@ package com.devpro.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.devpro.R;
 
 public class RequestsCompanyFragment extends Fragment {
-    Button addLocation, addDescription, addService, changePhoneNumber;
-    Button changeEmail;
+    CardView cardItem;
 
     public RequestsCompanyFragment() {
     }
@@ -22,7 +23,26 @@ public class RequestsCompanyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.company_homepage_requests, container, false);
+        return inflater.inflate(R.layout.requests_list_owner_location, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        cardItem = requireActivity().findViewById(R.id.cardView);
+        setListenersButtons();
+    }
+
+    private void setListenersButtons() {
+        cardItem.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
     }
 
     @Override
